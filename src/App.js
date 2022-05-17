271a443e398018ce891018d978ecd2af105225b7
javgonzalez <javgonzalez@csumb.edu>
2022-05-16 17:26:15 -0700

    added addAssignment route & component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import './App.css';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Gradebook from './components/Gradebook';
-import Assignment from './components/Assignment';
-import {BrowserRouter, Switch,  Route} from 'react-router-dom';
+import './App.css'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Gradebook from './components/Gradebook'
+import Assignment from './components/Assignment'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <AppBar position="static" color="default">
+    <div className='App'>
+      <AppBar position='static' color='default'>
         <Toolbar>
-           <Typography variant="h6" color="inherit">
+          <Typography variant='h6' color='inherit'>
             Gradebook
-           </Typography>
+          </Typography>
         </Toolbar>
       </AppBar>
       <BrowserRouter>
-       <Switch>
-        <Route exact path='/' component={Assignment} />
-        <Route path='/gradebook' component={Gradebook} />
-       </Switch>
+        <Switch>
+          <Route exact path='/' component={Assignment} />
+          <Route path='/gradebook' component={Gradebook} />
+          {/* new route to add assignment */}
+          <Route path='/assignment' component={Assignment} />
+        </Switch>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
